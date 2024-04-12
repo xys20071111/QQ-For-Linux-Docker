148d7c36ca0f85b8070528d01a8a7a36aaf11eac
xys20071111 <xys20071111@hotmail.com>
2024-04-12 17:12:26 +0800

    给qq的启动参数加上--no-sandbox\n不知道这样会不会对安全性产生影响\n但是这么做就不用特权容器了

--- run_in_image.sh
+++ run_in_image.sh
@@ -4,4 +4,4 @@ echo "user:123456" | chpasswd
 chown user:user /home/user/
 chown user:user /run/user/$(id -u user)
 su -c "notify-send 如果你能看到这句话 那dbus就应该配置好了" user
-su -c "qq" user
+su -c "qq --no-sandbox" user
